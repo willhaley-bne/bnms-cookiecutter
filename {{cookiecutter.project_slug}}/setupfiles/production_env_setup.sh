15353138c4b9cd346c1942d93efc7c5b8abded15
Will <willhaley@boddienoell.com>
2019-07-15 11:18:52 -0400

    Making small changes based on deploying a service

--- {{cookiecutter.project_slug}}/setupfiles/production_env_setup.sh
+++ {{cookiecutter.project_slug}}/setupfiles/production_env_setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-mkdir /var/bnms/{{cookiecutter.project_name}}
 cd /var/bnms/{{cookiecutter.project_name}}
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements
+pip install -r requirements.txt
 cp setupfiles/{{cookiecutter.app_name}}.conf /etc/httpd/conf.d/.
+semanage port -a -t http_port_t -p tcp {{cookiecutter.port_number}}
+firewall-cmd --zone=public --add-port={{cookiecutter.port_number}}/tcp --permanent
 chown -R apache:apache /var/bnms/{{cookiecutter.project_name}}
 
+echo "Now verify settings, append the proxy configs, and restart httpd"
